e40c0b9a5a0357d70517dd90cde9460712f12dbf
Hugo <hugobqdweb@laposte.net>
2022-05-11 16:02:36 +0200

    add rollup-plugin-analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import analyze from "rollup-plugin-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [analyze({ summaryOnly: true })],
+    },
+  },
+});
